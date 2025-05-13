82b9410fe5ae42c359dcf430be8dbf8b793bae81
ALISAEED333 <202002468@stu.edu.uob.bh>
2025-05-13 07:03:28 -0700

    git commit -m "commit message"
    git push

--- Home/script.js
+++ Home/script.js
@@ -1,40 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var header = document.getElementById('myHeader');
-	  var page = document.getElementById('page');
-    var openMenuButton = document.getElementById('openmenu');
+document.addEventListener('DOMContentLoaded', function () {
+  var header = document.getElementById('myHeader');
+  var page = document.getElementById('page');
+  var openMenuButton = document.getElementById('openmenu');
 
-    window.addEventListener('scroll', function() {
-				page.classList.remove('menuopen');
-        if (window.scrollY >= 100) {
-            header.classList.add('sticky');
-        } else {
-            header.classList.remove('sticky');
-        }
-    });
+  window.addEventListener('scroll', function () {
+    page.classList.remove('menuopen');
+    if (window.scrollY >= 100) {
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky');
+    }
+  });
 
-    // Event listener to remove the sticky class when the button is clicked
-    openMenuButton.addEventListener('click', function() {
-        header.classList.remove('sticky');
-				page.classList.add('menuopen');
-    });
-	
-	var links = document.querySelectorAll('a[href^="#"]');
+  // Event listener to remove the sticky class when the button is clicked
+  openMenuButton.addEventListener('click', function () {
+    header.classList.remove('sticky');
+    page.classList.add('menuopen');
+  });
 
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            // Prevent the default action
-            event.preventDefault();
+  var links = document.querySelectorAll('a[href^="#"]');
 
-            // Get the target element
-            var targetId = this.getAttribute('href');
-            var targetElement = document.querySelector(targetId);
+  links.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      // Prevent the default action
+      event.preventDefault();
 
-            // Smooth scroll to target
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
+      // Get the target element
+      var targetId = this.getAttribute('href');
+      var targetElement = document.querySelector(targetId);
+
+      // Smooth scroll to target
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
         });
+      }
     });
+  });
 });
