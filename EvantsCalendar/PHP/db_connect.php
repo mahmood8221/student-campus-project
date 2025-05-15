8f8b43bd04a3d54c3f92217ed32d9be83a49923c
Mohammed112m <mh5205897@gmail.com>
2025-05-15 10:58:04 +0300

    Update

--- EvantsCalendar/PHP/db_connect.php
+++ EvantsCalendar/PHP/db_connect.php
@@ -4,7 +4,7 @@ $host = "127.0.0.1";
 $user = getenv("db_user");
 $pass = getenv("db_pass");
 $db = getenv("db_name");
-
+ 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
